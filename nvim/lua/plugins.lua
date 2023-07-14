5e616e8f69f0f830d25937e2d251ef51d2a99600
Collin L <lkangn.collin@gmail.com>
2023-07-14 15:39:29 +0800

    modified:   nvim/lua/plugins.lua 	modified:   zsh/zshrc

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -434,18 +434,18 @@ require("lazy").setup({
 	-- 	},
 	-- },
 
-	{
-		"ggandor/flit.nvim",
-		opts = {
-			keys = { f = 'f', F = 'F', t = 't', T = 'T' },
-			-- A string like "nv", "nvo", "o", etc.
-			labeled_modes = "v",
-			multiline = true,
-			-- Like `leap`s similar argument (call-specific overrides).
-			-- E.g.: opts = { equivalence_classes = {} }
-			opts = {}
-		}
-	},
+	-- {
+	-- 	"ggandor/flit.nvim",
+	-- 	opts = {
+	-- 		keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+	-- 		-- A string like "nv", "nvo", "o", etc.
+	-- 		labeled_modes = "v",
+	-- 		multiline = true,
+	-- 		-- Like `leap`s similar argument (call-specific overrides).
+	-- 		-- E.g.: opts = { equivalence_classes = {} }
+	-- 		opts = {}
+	-- 	}
+	-- },
 
 	-- Marco
 	{
