30fd2c3261c728d57ea428b56706f5a145536e45
Collin L <lkangn.collin@gmail.com>
2023-07-17 17:15:35 +0800

    feat(neovim): add cscope plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -437,6 +437,17 @@ require("lazy").setup({
 	-- 		opts = {}
 	-- 	}
 	-- },
+	
+	-- cscope
+	{
+		"dhananjaylatkar/cscope_maps.nvim",
+		dependencies = {
+			"folke/which-key.nvim", -- optional [for whichkey hints]
+			"nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
+			"ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
+			"nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
+		},
+	},
 
 	-- Marco
 	{
