85d61a3b1bf26cc8b13b468a21d37e58141c389a
Collin L <lkangn.collin@gmail.com>
2023-08-11 16:40:52 +0800

    feat(neovim): update

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -47,7 +47,7 @@ vim.o.signcolumn='yes'
 vim.o.termguicolors=true
 
 -- 显示不可见字符格式，set list打开，set nolist关闭
-vim.o.listchars="eol:$,tab:>-,trail:-,extends:>,precedes:<,nbsp:+"
+vim.o.listchars="eol:$,tab:>-,trail:·,extends:⟩,precedes:⟨,nbsp:␣"
 
 -- 系统粘贴板
 vim.o.clipboard=vim.o.clipboard .. "unnamedplus"
