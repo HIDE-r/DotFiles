827137229bb5099c0ca9f0c3d35e1dbb9c4825fc
Collin L <lkangn.collin@gmail.com>
2023-09-05 11:06:21 +0800

    feat(neovim): add csope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -90,7 +90,7 @@ require("lazy").setup({
 	{
 		'sindrets/diffview.nvim',
 		enabled = false,
-		dependencies = 'nvim-lua/plenary.nvim',
+		-- dependencies = 'nvim-lua/plenary.nvim',
 	},
 
 	{
@@ -418,6 +418,7 @@ require("lazy").setup({
 		-- },
 	-- },
 
+	-- Scroll
 	-- {
 	-- 	'karb94/neoscroll.nvim',
 	-- 	event = "BufRead",
@@ -503,6 +504,22 @@ require("lazy").setup({
 			"ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
 			"nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
 		},
+		opts = {
+			cscope = {
+				picker = "telescope", -- "telescope", "fzf-lua" or "quickfix"
+			}
+		},
+	},
+	{
+		"ludovicchabant/vim-gutentags",
+		after = "cscope_maps.nvim",
+		init = function()
+			vim.g.gutentags_modules = {"cscope_maps"} -- This is required. Other config is optional
+			vim.g.gutentags_cscope_build_inverted_index_maps = 1
+			vim.g.gutentags_cache_dir = vim.fn.expand("~/code/.gutentags")
+			vim.g.gutentags_file_list_command = "fd -e c -e h"
+			-- vim.g.gutentags_trace = 1
+		end,
 	},
 
 	-- Marco
