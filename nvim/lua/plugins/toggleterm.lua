ae19aaf0798fe5eaf422d24c552db15ca7e2578c
Collin L <lkangn.collin@gmail.com>
2024-04-01 22:10:28 +0800

    feat(neovim): update toggleterm config

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -8,14 +8,18 @@ return {
 		version = "*",
 		init = function()
 			function _G.set_terminal_keymaps()
-				local opts = {buffer = 0}
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = 0
+					vim.keymap.set(mode, l, r, opts)
+				end
 				-- vim.keymap.set('t', '<C-[>', [[<C-\><C-n>]], opts)
-				vim.keymap.set('t', '<C-q>', [[<C-\><C-n>]], opts)
-				vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-				vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-				vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-				vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-				vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+				map('t', '<C-q>', [[<C-\><C-n>]], { desc = "[toggleterm] normal mode"})
+				map('t', '<C-h>', [[<Cmd>wincmd h<CR>]], { desc = "[toggleterm] left"})
+				map('t', '<C-j>', [[<Cmd>wincmd j<CR>]], { desc = "[toggleterm] down"})
+				map('t', '<C-k>', [[<Cmd>wincmd k<CR>]], { desc = "[toggleterm] up"})
+				map('t', '<C-l>', [[<Cmd>wincmd l<CR>]], { desc = "[toggleterm] right"})
+				map('t', '<C-w>', [[<C-\><C-n><C-w>]], { desc = "[toggleterm] select window"})
 			end
 
 			-- if you only want these mappings for toggle term use term://*toggleterm#* instead
