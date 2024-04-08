9ba0f143876acc23656dccff374fe9e3aecd0d11
Collin L <lkangn.collin@gmail.com>
2024-04-08 15:58:22 +0800

    feat(neovim): update

---
+++ nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"shellRaining/hlchunk.nvim",
+		event = { "UIEnter" },
+		opts = {
+			line_num = {
+				enable = false,
+			},
+			indent = {
+				enable = false,
+			},
+			blank = {
+				enable = false,
+			}
+		},
+	},
+}
