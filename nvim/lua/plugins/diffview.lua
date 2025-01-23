8fce247ef2e8ebc8d2e43508bd9cbb1dc662995e
Collin L <lkangn.collin@gmail.com>
2025-01-23 15:19:12 +0800

    feat(neovim): add color to current line at diff mode
    Change-Id: I3fc926bbc3f67cf6c39cd47820503058abae0b9d

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,6 +1,6 @@
 return {
 	{
 		'sindrets/diffview.nvim',
-		enabled = false
+		-- enabled = false
 	},
 }
--- nvim/lua/plugins/modes.lua
+++ nvim/lua/plugins/modes.lua
@@ -1,8 +1,17 @@
 return {
 	{
 		'mvllow/modes.nvim',
-		enabled = false,
-		version = "v0.2.0",
-		opts = {},
+		enabled = true,
+		version = "v0.2.1",
+		opts = {
+			colors = {
+				bg = "", -- Optional bg param, defaults to Normal hl group
+				copy = "#f5c359",
+				delete = "#c75c6a",
+				insert = "#78ccc5",
+				-- visual = "#9745be",
+				visual = "#989848",
+			},
+		},
 	}
 }
