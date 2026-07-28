3c87d676c7c798058530d064615006b0c2e916f8
Collin L <lkangn.collin@gmail.com>
2026-07-28 19:24:21 +0800

    refactor(treesitter): 暂停自定义解析器与高亮配置
    - 注释解析器自动安装列表
    - 禁用 Treesitter 配置初始化函数

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,22 +3,22 @@ return {
 		'nvim-treesitter/nvim-treesitter',
 		lazy = false,
 		build = ':TSUpdate',
-		opts = {
-			ensure_installed = {
-				"c", "cpp", "lua", "yaml", "markdown", "rust"
-			},
-			-- auto_install = true,
-			highlight = {
-				enable = true,
-		-- 		additional_vim_regex_highlighting = false,
-			},
-		-- 	matchup = {
-		-- 		enable = true,              -- mandatory, false will disable the whole extension
+		-- opts = {
+		-- 	ensure_installed = {
+		-- 		"c", "cpp", "lua", "yaml", "markdown", "rust"
 		-- 	},
-		},
-		config = function(_, opts)
-			require'nvim-treesitter.configs'.setup(opts)
-		end
+		-- 	-- auto_install = true,
+		-- 	highlight = {
+		-- 		enable = true,
+		-- -- 		additional_vim_regex_highlighting = false,
+		-- 	},
+		-- -- 	matchup = {
+		-- -- 		enable = true,              -- mandatory, false will disable the whole extension
+		-- -- 	},
+		-- },
+		-- config = function(_, opts)
+		-- 	require'nvim-treesitter.configs'.setup(opts)
+		-- end
 	},
 
 	{
