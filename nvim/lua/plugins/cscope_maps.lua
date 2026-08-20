276cc39a30f690002c24bf7893f9ed3628939637
Collin L <lkangn.collin@gmail.com>
2026-08-20 13:59:47 +0800

    perf(cscope): 添加命令与快捷键懒加载配置
    - 注册 Cscope 相关命令和查找快捷键
    - 禁用未使用的 fzf-lua 可选依赖

--- nvim/lua/plugins/cscope_maps.lua
+++ nvim/lua/plugins/cscope_maps.lua
@@ -10,10 +10,24 @@
 return {
 	{
 		"dhananjaylatkar/cscope_maps.nvim",
+		cmd = { "Cscope", "Cs", "Cstag", "CsPrompt", "CsStackView" },
+		keys = {
+			{ "<leader>cs", mode = { "n", "v" }, desc = "[cscope] find symbol" },
+			{ "<leader>cg", mode = { "n", "v" }, desc = "[cscope] find definition" },
+			{ "<leader>cc", mode = { "n", "v" }, desc = "[cscope] find callers" },
+			{ "<leader>ct", mode = { "n", "v" }, desc = "[cscope] find text" },
+			{ "<leader>ce", mode = { "n", "v" }, desc = "[cscope] find egrep pattern" },
+			{ "<leader>cf", mode = { "n", "v" }, desc = "[cscope] find file" },
+			{ "<leader>ci", mode = { "n", "v" }, desc = "[cscope] find includes" },
+			{ "<leader>cd", mode = { "n", "v" }, desc = "[cscope] find callees" },
+			{ "<leader>ca", mode = { "n", "v" }, desc = "[cscope] find assignments" },
+			{ "<leader>cb", mode = { "n", "v" }, desc = "[cscope] build database" },
+			{ "<C-]>", mode = { "n", "v" }, desc = "[cscope] cstag" },
+		},
 		dependencies = {
 			"folke/which-key.nvim", -- optional [for whichkey hints]
 			"nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
-			"ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
+			-- "ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
 			"nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
 		},
 		opts = {
